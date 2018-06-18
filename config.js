512300aeaafaafe8df53ce0ee6775037aa0d2e2a
thabtom <tsehayegh@gmail.com>
2018-06-18 19:09:58 -0400

    changed client origin to localhost:3000

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/my-acad-roadmap';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-my-acad-roadmap';
 exports.PORT = process.env.PORT || 8080;
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN  || 'https://my-acad-roadmap.herokuapp.com' ;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN  || 'http://localhost:3000' ;
 
 
 exports.JWT_SECRET = process.env.JWT_SECRET;
