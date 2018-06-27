e021dd1cdd8b4c71216c086fe6d0c28757904648
thabtom <tsehayegh@gmail.com>
2018-06-27 16:52:18 -0400

    cleaned code lines

--- client/src/components/acadplan/acadPlansDashboard.test.js
+++ client/src/components/acadplan/acadPlansDashboard.test.js
@@ -3,6 +3,33 @@ import {shallow, mount} from 'enzyme';
 
 import {AcadPlanDashboard} from './acadPlansDashboard';
 
+import {
+	FETCH_CATALOG_SUCCESS,
+	fetchCatalogSuccess,
+	fetchCatalog,
+	FETCH_ACAD_PLANS_SUCCESS,
+	fetchAcadPlansSuccess,
+	fetchAcadPlans,
+	setSemester,
+	SET_SEMESTER,
+	setYear,
+	SET_YEAR
+    } from '../../actions/catalogActions';
+
+const mockFetchAcadPlansSuccessAction = {
+	type: 'FETCH_ACAD_PLANS_SUCCESS'
+}
+
+jest.mock('../../actions/catalogActions', () => Object.assign({},
+	require.requireActual('../../actions/catalogActions'),
+	{
+		fetchAcadPlans: jest.fn().mockImplementation(() => {
+			return mockFetchAcadPlansSuccessAction;
+		})
+	}
+))
+
+
 describe('<AcadPlanDashboard', () => {
 	it('Renders without crashing', () => {
 
@@ -10,7 +37,7 @@ describe('<AcadPlanDashboard', () => {
 	});
 
 	it('Renders without crashing', () => {
-		const wrapper = shallow(<AcadPlanDashboard />);
+		const wrapper = shallow(<AcadPlanDashboard fetchAcadPlans={jest.fn()} />);
 		console.log(wrapper);
 		expect(wrapper.hasClass('container').toEqual(true));
 	})
